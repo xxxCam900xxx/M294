1199e0bf5d50c1258e30485c7f4219835e2f13d1
Cam <112639963+CameronMeile@users.noreply.github.com>
2024-05-17 13:16:53 +0200

    chore: Auftrag 2.4 mini changes

--- Modul Tag 1/Block_02/Auftrag 2.4/Content/script.js
+++ Modul Tag 1/Block_02/Auftrag 2.4/Content/script.js
@@ -1,7 +1,8 @@
 const cantons = ['ag', 'ar', 'bl', 'fr', 'gl', 'ju', 'ne', 'ow', 'sh', 'sz', 'ti', 'vd', 'zg', 'ai', 'be', 'bs', 'ge', 'gr', 'lu', 'nw', 'sg', 'so', 'tg', 'ur', 'vs', 'zh'];
+const playground = document.getElementById('playground');
 
 const getFlagPath = function (canton) {
-  return `/Modul Tag 1/Block_02/Auftrag 2.4/Content/img/${canton}.png`;
+  return `./img/${canton}.png`;
 };
 
 // Funktion zum Mischen eines Arrays
@@ -13,34 +14,31 @@ const shuffleArray = function (array) {
   return array;
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-  const playground = document.getElementById('playground');
+// Zufällige Auswahl von 10 Kantonen
+const randomCantons = shuffleArray(cantons).slice(0, 10);
 
-  // Zufällige Auswahl von 10 Kantonen
-  const randomCantons = shuffleArray(cantons).slice(0, 10);
+// Durchgang zwei Mal ausführen
+for (let i = 0; i < 2; i++) {
+  // Zufällige Reihenfolge für die 10 Kantonen-Bilder
+  const shuffledCantons = shuffleArray(randomCantons);
 
-  // Durchgang zwei Mal ausführen
-  for (let i = 0; i < 2; i++) {
-    // Zufällige Reihenfolge für die 10 Kantonen-Bilder
-    const shuffledCantons = shuffleArray(randomCantons);
+  shuffledCantons.forEach(function (value) {
+    const tile = document.createElement('div');
+    const tileButton = document.createElement('button');
+    const tileImage = document.createElement('img');
 
-    shuffledCantons.forEach(function (value) {
-      const tile = document.createElement('div');
-      const tileButton = document.createElement('button');
-      const tileImage = document.createElement('img');
+    // Bild zunächst verdecken
+    tileImage.classList.add('hidden');
 
-      // Bild zunächst verdecken
-      tileImage.classList.add('hidden');
-
-      tileButton.addEventListener('click', function () {
-        // Bei Klick auf den Button das Bild anzeigen
-        tileImage.classList.remove('hidden');
-      });
-
-      tileImage.setAttribute('src', getFlagPath(value));
-      tileButton.appendChild(tileImage);
-      tile.appendChild(tileButton);
-      playground.appendChild(tile);
+    tileButton.addEventListener('click', function () {
+      // Bei Klick auf den Button das Bild anzeigen
+      tileImage.classList.remove('hidden');
+      comparePair();
     });
-  }
-});
+
+    tileImage.setAttribute('src', getFlagPath(value));
+    tileButton.appendChild(tileImage);
+    tile.appendChild(tileButton);
+    playground.appendChild(tile);
+  });
+}
