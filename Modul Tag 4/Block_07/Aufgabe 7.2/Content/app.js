4419aaf9c74ec4e6bf75fb9c8b7ec6c2f7a7ec23
Cam <112639963+CameronMeile@users.noreply.github.com>
2024-05-30 15:47:51 +0200

    chore: Aufgabe 7.2

--- Modul Tag 4/Block_07/Aufgabe 7.2/Content/app.js
+++ Modul Tag 4/Block_07/Aufgabe 7.2/Content/app.js
@@ -2,6 +2,8 @@ const card = document.forms.register;
 const message = card.elements.message;
 const user = card.elements.username;
 
+const output = document.getElementById('output');
+
 card.addEventListener('submit', async (event) => {
     event.preventDefault();
 
@@ -19,17 +21,51 @@ card.addEventListener('submit', async (event) => {
 
     const myMessage = await fetch('http://10.65.4.20/comments', {
         method: 'POST',
-        headers: { 
+        headers: {
             "Content-Type": "application/json",
-            "Authorization" : Auth,
-         },
+            "Authorization": Auth,
+        },
         body: JSON.stringify(
             {
-                "message" : message.value,
-                "username" : user.value,
+                "message": message.value,
+                "username": user.value,
             }
         ),
     });
 
-
 });
+
+// Store the interval ID in a variable
+const intervalId = setInterval(async function() {
+    try {
+      const response = await fetch('http://10.65.4.20/comments', {
+        method: 'GET',
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+  
+        output.innerHTML = "";
+  
+        data.forEach(element => {
+          let mes = document.createElement('div');
+          let message = document.createElement('p');
+          let User = document.createElement('h1');
+  
+          message.innerText = element.message;
+          User.innerText = element.username;
+  
+          mes.append(User);
+          mes.append(message);
+          output.append(mes);
+        });
+  
+      } else {
+        console.log('Fehler beim Abrufen der Daten. Status:', response.status);
+        clearInterval(intervalId); // Stop the interval
+      }
+    } catch (error) {
+      console.log('Fehler aufgetreten:', error);
+      clearInterval(intervalId); // Stop the interval
+    }
+  }, 1000);
